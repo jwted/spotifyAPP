619e8b41b224642c16bf928db42d8a7426e06e0c
Taísa Alho <92999297+taisaalho@users.noreply.github.com>
2023-11-20 12:15:02 +0000

    Fix no router

---
+++ spotify-App/src/views/LandingView.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <router-link to="/home" class="pr-3">HOME</router-link><br>
+        <router-link to="/login" class="pr-3">LOGIN</router-link>
+    </div>
+</template>
+
+<script>
+    import {RouterLink} from "vue-router"
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
